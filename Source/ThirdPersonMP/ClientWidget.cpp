f2416715d2ffd2f509e5813e8650f371f4ee64ae
vernon-roche <gothic00742@wp.pl>
2020-03-07 22:56:10 +0100

    Initial commit

---
+++ Source/ThirdPersonMP/ClientWidget.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClientWidget.h"
+#include "Components/TextBlock.h"
+
+void UClientWidget::SetClientText(FString NewText)
+{
+	HealthText->SetText(FText::FromString(NewText));
+}
